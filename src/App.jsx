7f8b3fa14431616f78f70036dc47cda6c9fe2232
Vivek Tarnallya <116621113+vivekbeast@users.noreply.github.com>
2025-01-03 18:57:46 +0530

    hmmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // import { useState } from 'react'
 import './App.css'
+import Home from './components/Home'
 // import { BrowserRouter } from "react-router-dom";
 // import { Route, Routes } from "react-router-dom";
-import Home from './components/home';
+
 function App() {
 
   return (
